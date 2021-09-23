7cc48184584ab55a585ff725477ee1a67b1a190c
AaronKylePorche <AaronKylePorche@gmail.com>
2021-09-22 19:19:07 -0700

    refactored much of the code, made opening and closing accordion smooth, simplified layout complexity

--- script/script.js
+++ script/script.js
@@ -1,40 +1,49 @@
 window.onload = function() {
-    const arrow = document.getElementsByClassName("arrow")
-    const question = document.getElementsByClassName("question-container")
-
+    const arrow = document.getElementsByClassName("arrow1")
+    const question = document.getElementsByClassName("question-container1")
+    const panel = document.getElementsByClassName("answer1")
+    
+    
+    
+    
+     
     
 
-
-
+ //makes question clickable   
     for (let i = 0; i < question.length; i++) {
+          
+
+        question[i].addEventListener('click', function(){
+            console.log(question[i])
+            this.classList.toggle('question-active')
+            this.lastElementChild.classList.toggle('arrow-rotate') 
+            const panel = this.nextElementSibling
+            if (panel.style.maxHeight){
+                panel.style.maxHeight = null;
+                }   else{
+                        panel.style.maxHeight = panel.scrollHeight + "px"        
+            
+                        }
+            })
         
-        question[i].addEventListener('click', function() {
+    }
+//makes answer clickable
+    for (let i = 0; i <panel.length; i++) {
+        
+        panel[i].addEventListener('click', function(){
             
-            console.log('click');
-
-            arrow[i].classList.toggle("arrow-rotate")
-                                
-            let answer = this.children[1];
+            if (this.style.maxHeight){
+                this.style.maxHeight = null;
+                }   else{
+                        panel.style.maxHeight = panel.scrollHeight + "px"        
             
-            answer.classList.toggle("answer__open")
-            
-            this.classList.toggle("question__active")
-
-            
-            
-            
- 
-
-            
-
-            
+                        }
+            this.previousElementSibling.children[1].classList.toggle('arrow-rotate') 
+            console.log(this)
+            this.previousElementSibling.classList.toggle('question-active')                
         })
     }
 
 }
-
-
-
-
-
-
+        
+    
